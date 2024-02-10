0af1a5f17108a5312c3bb285ecd17a502475b5c8
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-10 12:09:56 -0500

    feat: project setup and initial screen

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+        centerTitle: true,
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // how many columns
+          childAspectRatio: 3 / 2, // size of each grid element
+          crossAxisSpacing: 20, // spacing between the columns
+          mainAxisSpacing: 20, // spacing between the rows
+        ),
+        children: [
+          for (final index in [1, 2, 3, 4, 5, 6])
+            Center(
+              child: Text(
+                index.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
