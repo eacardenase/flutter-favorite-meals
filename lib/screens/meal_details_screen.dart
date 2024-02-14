80c81fb5d20532fdab1568532fa0b4d7d0f5e813
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-14 09:24:08 -0500

    feat: use implicit animation with favorite icon

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -46,8 +46,19 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(
-              isFavorite ? Icons.star : Icons.star_border,
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 150),
+              transitionBuilder: (child, animation) => RotationTransition(
+                turns: Tween<double>(
+                  begin: 0.65,
+                  end: 1.0,
+                ).animate(animation),
+                child: child,
+              ),
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
             ),
           )
         ],
